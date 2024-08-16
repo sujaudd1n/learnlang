0c5f4f87e4fc02ea0b3a78333565dc0fe3e63257
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2024-08-16 12:14:32 +0530

    Improve header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react"
-import FullWidthLink from "./FullWidthLink"
+import { css } from "@emotion/react";
+import Button from '@mui/joy/Button';
+import { Stack } from "@mui/joy";
+
 
 export default function Header() {
     return (
         <>
             <header
                 css={css`
+                padding: 40px  0;
+                margin-bottom: 80px;
             `}
             >
                 <h1
@@ -14,18 +18,39 @@ export default function Header() {
             text-align: center;
             overflow-wrap: break-word;
             color: transparent;
-            background-image: linear-gradient(to right, var(--text), var(--accent));
+            background-image: linear-gradient(to bottom, var(--text), var(--accent));
             background-clip: text;
-            font-size: calc(2rem + 3dvw);
-            margin: calc(20px + 5dvw) auto;
+            font-size: max(48px, min(5vw, 70px));
+            font-weight: 800;
+            margin: 0 auto;
+            padding: 40px 0;
             max-width: max(80dvw, 500px);
             `}
                 >
-                    Simple yet Comprehensive guide to learn a new language.</h1>
-                <FullWidthLink
-                    href="#selectlang"
-                    text="</> Choose Your Language and Get Started!"
-                />
+                    Simple and efficient way to learn a new programming language
+                </h1>
+                <p
+                    css={css`
+                    line-height: 1.5rem;
+                    text-align: center;
+                    padding: 40px;
+                    margin: 0 auto;
+                    max-width: 700px;
+                    `}
+                >
+                    From the basics of typing systems to advanced testing and debugging
+                    techniques, our comprehensive guide will walk you through the entire process of learning a new language.
+                </p>
+                <Stack direction="row"
+                justifyContent="center"
+                spacing={2}
+                css={css`
+                padding: 30px 0;
+                `}
+                >
+                    <Button>Get Started</Button>
+                    <Button variant="soft">Select Language</Button>
+                </Stack>
             </header>
         </>
     )
--- src/components/LanguageDetail.jsx
+++ src/components/LanguageDetail.jsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
 import { useEffect, useState } from "react"
-import Showdown from "showdown"
+import Showdown from "showdown";
 
 const converter = new Showdown.Converter();
 
