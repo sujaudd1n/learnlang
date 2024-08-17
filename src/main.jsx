722840ab94904557149f38075f59af5463300ea1
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2024-08-18 01:06:37 +0530

    Add ScrollToTop component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import './index.css'
+import ScrollToTop from './components/ScrollToTop'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </React.StrictMode>,
