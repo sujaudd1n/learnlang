ce034009d3b57b95deae33d0ff45e51e176576cf
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2024-08-19 16:16:42 +0530

    Replace BrowserRouter with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter, BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import ScrollToTop from './components/ScrollToTop'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <ScrollToTop />
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
 )
